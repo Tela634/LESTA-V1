05537b4c1eb9cf79af80291886f30def8ab10188
Fetal01 <eliudnjora53@gmail.com>
2025-04-07 19:57:43 +0300

    Update ownerConfig.js

--- media/settings/ownerConfig.js
+++ media/settings/ownerConfig.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 // gausah di ubah rek karna
 // udh ada request pas di scan pairing
 global.ownername = 'xZiyy';
-global.owner = '6283159451487';
-global.botname = 'clairity bot';
+global.owner = '254706519089';
+global.botname = 'ELIUD_LESTA';
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
