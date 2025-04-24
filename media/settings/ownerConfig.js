be897facfd14df450ccec4e8591ff298edffbbd3
Tela634 <veronicawaithira680@gmail.com>
2025-04-24 16:08:11 +0300

    Update ownerConfig.js

--- media/settings/ownerConfig.js
+++ media/settings/ownerConfig.js
@@ -2,8 +2,8 @@
 const fs = require('fs')
 // gausah di ubah rek karna
 // udh ada request pas di scan pairing
-global.ownername = 'xZiyy';
-global.owner = '254706519089';
+global.ownername = 'Lesta_Eliud';
+global.owner = '254701309409';
 global.botname = 'ELIUD_LESTA';
 
 let file = require.resolve(__filename)
